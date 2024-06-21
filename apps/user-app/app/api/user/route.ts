218419de0e918a79f9b52639aeb10c94d4e0d70b
Sunny <swarnendughosh05@gmail.com>
2024-06-21 13:40:46 +0530

    preject bootsraped & webhook init

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,15 +3,21 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
-    if (session.user) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (session.user) {
+            return NextResponse.json({
+                user: session.user
+            })
+        }
+    } catch (e) {
+        console.error(e);
         return NextResponse.json({
-            user: session.user
+            message: "You are not logged in"
+        }, {
+            status: 403
         })
     }
-    return NextResponse.json({
-        message: "You are not logged in"
-    }, {
-        status: 403
-    })
+  
+    
 }
