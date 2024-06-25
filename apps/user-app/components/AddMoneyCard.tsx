f678de9e0101016e4eb4ff7b9d168c49d80e3270
Sunny <swarnendughosh05@gmail.com>
2024-06-25 12:05:22 +0530

    bug fixed

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -22,7 +22,7 @@ export const AddMoney = () => {
     return <Card title="Add Money">
     <div className="w-full">
         <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value) => {
-            setamount(value);
+            setamount(Number(value));
 
         }} />
         <div className="py-4 text-left">
