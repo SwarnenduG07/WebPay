9a00d09e0e0ffc49ae4183a49cb26f43a055102f
Sunny <swarnendughosh05@gmail.com>
2024-07-07 09:59:18 +0530

    SendCard completed

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -37,3 +37,11 @@ function TransactionsIcon() {
   </svg>
   
 }
+<SidebarItem href={"/p2p"} icon={<P2PTransferIcon />} title="P2P Transfer" />
+
+
+function P2PTransferIcon() {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+  </svg>
+}
